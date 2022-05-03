05c4be0a9c4ddae1d41cc67b978ce64f36246768
alexelizaga <alexelizaga@icloud.com>
2022-05-03 13:06:23 +0200

    72

--- src/GifApp.js
+++ src/GifApp.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const GifApp = () => {
+  const [categories, setCategories] = useState(['One Punch', 'Samurai X', 'Dragon Ball']);
+
+  const handleAdd = (newCategory) => {
+    setCategories([...categories, newCategory]);
+  };
+
   return (
     <>
       <h2>GifApp</h2>
       <hr />
+
+      <button onClick={() => handleAdd('HunterXHunter')}>Agregar</button>
+
+      <ol>
+        { 
+          categories.map(category => (
+            <li key={category}>{category}</li>
+          ))
+        }
+      </ol>
     </>
   )
 }
