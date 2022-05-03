97967cb773a8e71d485df7e2f1be7339ea01f459
alexelizaga <alexelizaga@icloud.com>
2022-05-03 17:16:26 +0200

    81

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,22 +1,18 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
+
+import { useFetchGifs } from '../hooks/useFetchGifs';
 import { GifGridItem } from './GifGridItem';
-import { getGifs } from '../helpers/getGifs';
 
 export const GifGrid = ({ category }) => {
-  const [images, setImages] = useState([]);
-
-  useEffect(() => {
-    getGifs(category)
-      .then( setImages);
-  }, [ category ]);
-  
-
-  
+  const { loading, data: images } = useFetchGifs(category);
 
   return (
     <>
       <h3>{ category }</h3>
+
+      { loading && <p>Loading...</p> }
+
       <div className='card-grid'>
         {
           images.map( img => (
@@ -27,6 +23,7 @@ export const GifGrid = ({ category }) => {
           ))
         }
       </div>
+
     </>
   )
 }
