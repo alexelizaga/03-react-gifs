54f365bf3d8695dfec504f0da3ec8d505cf08ca6
alexelizaga <alexelizaga@icloud.com>
2022-05-03 16:54:15 +0200

    79

--- src/components/AddCategory.js
+++ src/components/AddCategory.js
@@ -12,7 +12,7 @@ export const AddCategory = ({ setCategories }) => {
     e.preventDefault();
 
     if ( inputValue.trim().length < 3 ) return;
-    setCategories( cats => [...cats, inputValue]);
+    setCategories( cats => [inputValue,...cats]);
     setInputValue('');
     
   }
--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,31 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { GifGridItem } from './GifGridItem';
+import { getGifs } from '../helpers/getGifs';
 
 export const GifGrid = ({ category }) => {
   const [images, setImages] = useState([]);
 
   useEffect(() => {
-    getGifs();
-  }, []);
+    getGifs(category)
+      .then( setImages);
+  }, [ category ]);
   
 
-  const getGifs = async () => {
-    const url = 'https://api.giphy.com/v1/gifs/search?limit=10&q=Rick+and+Morty&api_key=ThjRH4OtNo7NKbCvpTb44j4dhB1tuM7a';
-    const resp = await fetch(url);
-    const { data } = await resp.json();
-
-    const gifs = data.map( img => {
-      return {
-        id: img.id,
-        title: img.title,
-        url: img.images?.downsized_medium.url,
-      }
-    });
-
-    console.log(gifs);
-    setImages(gifs);
-  }
+  
 
   return (
     <>
