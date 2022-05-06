d964df3c46dcd95480f78ecaeee43493d1a38458
alexelizaga <alexelizaga@icloud.com>
2022-05-06 13:22:40 +0200

    102

--- src/tests/hooks/useFetchGifs.test.js
+++ src/tests/hooks/useFetchGifs.test.js
@@ -1,5 +1,5 @@
 import '@testing-library/jest-dom';
-import { renderHook } from '@testing-library/react';
+import { renderHook, act } from '@testing-library/react-hooks';
 
 
 import { useFetchGifs } from '../../hooks/useFetchGifs';
@@ -12,6 +12,15 @@ describe('Test useFetchGifs', () => {
 
     expect( data ).toEqual([]);
     expect( loading ).toBeTruthy();
+  });
+
+  test('should return new state', async () => {
+    const { result, waitForNextUpdate } = renderHook( () => useFetchGifs('One Punch') );
+    await waitForNextUpdate();
+    
+    const { data, loading } = result.current;
+    expect( data.length ).toBe(10);
+    expect( loading ).toBeFalsy();
   });
 
 });
